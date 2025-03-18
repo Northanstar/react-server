7a5c3f6b52d297a0c1c1e64be1230ecf9947c5bf
Zeek <chigereisaac31@gmail.com>
2025-03-18 10:57:07 +0100

    update

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Products.css'
 
 function Product() {
   return (
-    <div>
-      <h2 style={{paddingTop: '13vh', color: 'white'}}>This is the products page </h2>
-    </div>
+    <div className="card-list">
+      <Link to="#" className="card-item">
+        <img src="src/assets/coffee-hero-bg.jpg" alt="card image"/><br/>
+        <span className="developer">Developer</span>
+        <h3>A "developer" codes software and websites</h3>
+        <div className="arrow">
+          <i className="fas fa-arrow-right card-icon"></i>
+        </div> <br />
+      </Link>
+
+   
+
+            <Link to="#" class="card-item">
+                <img src="src/assets/coffee-hero-bg.jpg" alt="card image"/><br/>
+                <span class="designer">Designer</span>
+                <h3>A "designer" is design expert</h3>
+                <div class="arrow">
+                    <i class="fas fa-arrow-right card-icon"></i>
+                </div> <br/>
+            </Link>
+
+                <Link to ="#" class="card-item">
+                    <img src="src/assets/coffee-hero-bg.jpg" alt="card image"/><br/>
+                    <span class="editor">Editor</span>
+                    <h3>An "editor" ensures content quality and accuracy</h3>
+                    <div class="arrow">
+                        <i class="fas fa-arrow-right card-icon"></i>
+                    </div> <br/>
+                </Link>
+               
+                    
+      </div>
   )
 }
 
